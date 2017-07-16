1c55044db7719d32cdadc2400bc4a090f9c69ba7
azam alias <azamalias@gmail.com>
2017-07-16 12:33:34 +0800

    added simpledb attribute creation and deletion operations

--- simpledb.py
+++ simpledb.py
@@ -26,3 +26,29 @@ class SimpleDB(object):
 
     def list_domains(self):
         return self.client.list_domains()
+
+    def put_attribute(self, domain_name, item_name, attribute):
+        return self.client.put_attributes(
+            DomainName=domain_name,
+            ItemName=item_name,
+            Attributes=[
+                {
+                    "Name": attribute["Name"],
+                    "Value": attribute["Value"],
+                    "Replace": True
+                }
+            ]
+        )
+
+    def get_item(self, domain_name, item_name):
+        return self.client.get_attributes(
+            DomainName=domain_name,
+            ItemName=item_name,
+            ConsistentRead=True
+        )
+
+    def delete_item(self, domain_name, item_name):
+        return self.client.delete_attributes(
+            DomainName=domain_name,
+            ItemName=item_name
+        )
--- test_simpledb.py
+++ test_simpledb.py
@@ -24,3 +24,21 @@ class SimpleDBIntegrationTestCase(unittest.TestCase):
         self.sdb.delete_domain(domain)
         domains = self.sdb.list_domains()
         self.assertNotIn(domain, str(domains))
+
+    def test_put_get_delete_generic_item(self):
+        domain = str(uuid.uuid4())
+        item_name = str(uuid.uuid4())
+        self.sdb.create_domain(domain)
+
+        attribute = {"Name": "attribute1name", "Value": "attribute1value"}
+        self.sdb.put_attribute(domain, item_name, attribute)
+
+        item = self.sdb.get_item(domain, item_name)
+        self.assertDictEqual(attribute, item["Attributes"][0])
+
+        self.sdb.delete_item(domain, item_name)
+        item2 = self.sdb.get_item(domain, item_name)
+        item2_attribute = item2.get("Attributes", "empty")
+        self.assertEqual(item2_attribute, "empty")
+
+        self.sdb.delete_domain(domain)
