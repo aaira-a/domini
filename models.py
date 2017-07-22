9e5421ea2bac12221f03c3a4fa98ab4ee7328677
azam alias <azamalias@gmail.com>
2017-07-22 22:43:05 +0800

    added failed count field to item model

--- models.py
+++ models.py
@@ -3,9 +3,14 @@ import requests
 
 class Item(object):
 
-    def __init__(self, url, token):
+    def __init__(self, url, token, is_existing=False, failed_count=0):
         self.url = url
         self.token = token
+        self.failed_count = failed_count
+
+        if not is_existing:
+            self.is_active = True
+            self.failed_count = 0
 
     def fetch_from_provider(self):
         try:
@@ -13,3 +18,4 @@ class Item(object):
             self.latest_call = requests.get(self.url, headers=headers)
         except Exception as e:
             self.latest_call = ("failed: " + str(e))
+            self.failed_count += 1
--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -15,12 +15,14 @@ class ItemTests(unittest.TestCase):
         self.headers = {"Authorization": token}
         self.item = Item(url, token)
 
-    def test_initialize_model_fields(self):
+    def test_initialize_model_fields_with_for_first_time(self):
         url = "https://abc/def/ghi?jkl=mno"
         token = "Bearer xyz"
 
         self.assertEqual(url, self.item.url)
         self.assertEqual(token, self.item.token)
+        self.assertTrue(self.item.is_active)
+        self.assertEqual(0, self.item.failed_count)
 
     @responses.activate
     def test_fetch_from_provider_with_correct_format(self):
@@ -49,3 +51,7 @@ class ItemTests(unittest.TestCase):
 
         self.item.fetch_from_provider()
         self.assertIn("failed", self.item.latest_call)
+        self.assertEqual(1, self.item.failed_count)
+
+        self.item.fetch_from_provider()
+        self.assertEqual(2, self.item.failed_count)
