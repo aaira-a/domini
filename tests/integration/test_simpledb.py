a9365080aa48576fcea9404ea55f75765eec3e52
azam alias <azamalias@gmail.com>
2017-07-27 23:09:17 +0800

    extracted setup and teardown for simpledb tests

--- tests/integration/test_simpledb.py
+++ tests/integration/test_simpledb.py
@@ -15,44 +15,48 @@ class SimpleDBIntegrationTests(unittest.TestCase):
             secret_key=os.environ["TEST_AWS_SECRET"]
         )
 
-    def test_create_list_delete_domain(self):
-        domain = str(uuid.uuid4())
-
-        self.sdb.create_domain(domain)
+    def setUp(self):
+        self.domain = str(uuid.uuid4())
+        self.sdb.create_domain(self.domain)
         domains = self.sdb.list_domains()
-        self.assertIn(domain, str(domains))
+        self.assertIn(self.domain, str(domains))
 
-        self.sdb.delete_domain(domain)
+    def tearDown(self):
+        self.sdb.delete_domain(self.domain)
         domains = self.sdb.list_domains()
-        self.assertNotIn(domain, str(domains))
+        self.assertNotIn(self.domain, str(domains))
 
     def test_put_get_delete_generic_item(self):
-        domain = str(uuid.uuid4())
         item_name = str(uuid.uuid4())
-        self.sdb.create_domain(domain)
 
         attributes = [
             {"Name": "attribute1name", "Value": "attribute1value"},
             {"Name": "attribute2name", "Value": "attribute2value"}
         ]
-        self.sdb.put_attributes(item_name, attributes, domain)
+        self.sdb.put_attributes(item_name, attributes, self.domain)
 
-        item = self.sdb.get_item(item_name, domain)
+        item = self.sdb.get_item(item_name, self.domain)
         difference = DeepDiff(attributes, item["Attributes"],
                               ignore_order=True)
         self.assertEqual({}, difference)
 
-        self.sdb.delete_item(item_name, domain)
-        item_recheck = self.sdb.get_item(item_name, domain)
+        self.sdb.delete_item(item_name, self.domain)
+        item_recheck = self.sdb.get_item(item_name, self.domain)
         if "Attributes" in item_recheck:
             self.fail("item should not contain any attribute")
 
-        self.sdb.delete_domain(domain)
+    def test_put_attribute_should_replace_previous_same_key(self):
+        item_name = str(uuid.uuid4())
+
+        attributes1 = [{"Name": "attribute1name", "Value": "attribute1value1"}]
+        attributes2 = [{"Name": "attribute1name", "Value": "attribute1value2"}]
+        self.sdb.put_attributes(item_name, attributes1, self.domain)
+        self.sdb.put_attributes(item_name, attributes2, self.domain)
+
+        item_recheck = self.sdb.get_item(item_name, self.domain)
+        self.assertEqual(attributes2, item_recheck["Attributes"])
 
     def test_query_more_than_one_items_by_attribute_value(self):
-        domain = str(uuid.uuid4())
-        self.sdb.create_domain(domain)
-
         item_1_name = str(uuid.uuid4())
         item_2_name = str(uuid.uuid4())
         item_3_name = str(uuid.uuid4())
@@ -60,11 +64,9 @@ class SimpleDBIntegrationTests(unittest.TestCase):
         value1 = {"Name": "attribute1name", "Value": "attribute1value1"}
         value2 = {"Name": "attribute1name", "Value": "attribute1value2"}
 
-        self.sdb.put_attributes(item_1_name, [value1], domain)
-        self.sdb.put_attributes(item_2_name, [value1], domain)
-        self.sdb.put_attributes(item_3_name, [value2], domain)
+        self.sdb.put_attributes(item_1_name, [value1], self.domain)
+        self.sdb.put_attributes(item_2_name, [value1], self.domain)
+        self.sdb.put_attributes(item_3_name, [value2], self.domain)
 
-        response = self.sdb.query("attribute1name", "attribute1value1", domain)
+        response = self.sdb.query("attribute1name", "attribute1value1", self.domain)
         self.assertEqual(2, len(response["Items"]))
-
-        self.sdb.delete_domain(domain)
