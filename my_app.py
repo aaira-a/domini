0452bc703325f8628bc7fe5c828ab86a1107f044
azam alias <azamalias@gmail.com>
2017-08-12 10:26:19 +0800

    added views for item add

--- my_app.py
+++ my_app.py
@@ -1,12 +1,31 @@
 
-from flask import Flask
+from flask import Flask, render_template, request
+
+import controllers
 
 app = Flask(__name__)
 
 
-@app.route("/", methods=['GET'])
+@app.route("/", methods=["GET"])
 def index():
     return "hello world"
+
+
+@app.route("/add-form", methods=["GET"])
+def add():
+    return render_template("add.html")
+
+
+@app.route("/add-post", methods=["POST"])
+def add_post():
+    try:
+        data = request.form
+        controller = controllers.ItemController()
+        controller.add(data["url"], data["token"], data["phone"])
+        return "great success!"
+
+    except Exception:
+        return "failed :'("
 
 
 if __name__ == "__main__":
--- tests/unit/test_my_app.py
+++ tests/unit/test_my_app.py
@@ -1,4 +1,6 @@
 import unittest
+from unittest.mock import patch
+
 from my_app import app
 
 
@@ -10,3 +12,28 @@ class AppTests(unittest.TestCase):
     def test_index_view_should_return_hello_world(self):
         response = self.app.get("/")
         self.assertIn(b"hello world", response.data)
+
+    def test_add_form_view_should_use_correct_template(self):
+        response = self.app.get("/add-form")
+        self.assertIn(b"Add item form", response.data)
+
+    @patch('my_app.controllers')
+    def test_add_post_view_should_call_model_controller(self, mock_module):
+        data = {"url": "myurl1", "phone": "+60123", "token": "mytoken1"}
+
+        self.app.post("/add-post", data=data)
+
+        mock_module.ItemController.assert_called()
+        mock_module.ItemController().add.assert_called_with(
+            "myurl1", "mytoken1", "+60123")
+
+    @patch('my_app.controllers')
+    def test_add_post_view_should_return_success_message(self, mock_module):
+        data = {"url": "myurl1", "phone": "+60123", "token": "mytoken1"}
+        response = self.app.post("/add-post", data=data)
+        self.assertIn(b"great success!", response.data)
+
+    @patch('my_app.controllers')
+    def test_add_post_view_should_return_failed_message(self, mock_module):
+        response = self.app.post("/add-post", data={})
+        self.assertIn(b"failed", response.data)
